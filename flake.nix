4e7f7fb58c96b68e54bdadd4dc52da51f6bd7d1d
Cole Helbling <cole.helbling@determinate.systems>
2021-11-08 08:41:09 -0800

    init flake.nix with outdated flake.lock

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "test";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+  };
+
+  outputs =
+    { nixpkgs
+    , self
+    , ...
+    }@inputs:
+    let
+      nameValuePair = name: value: { inherit name value; };
+      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+
+      pkgsFor = pkgs: system:
+        import pkgs { inherit system; };
+
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
+      forAllSystems = f: genAttrs allSystems
+        (system: f {
+          inherit system;
+          pkgs = pkgsFor nixpkgs system;
+        });
+    in
+    {
+      pkgs = forAllSystems ({ pkgs, ... }: pkgs);
+    };
+}
